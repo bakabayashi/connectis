185a379ce5855f6fe652b7340ec2b22a0ba83188
roman.szczykutowicz <roman.szczykutowicz@intive.com>
2018-06-29 13:51:45 +0200

    Empty, running application.

---
+++ src/main/java/com/connectis/codingtask/CodingTaskApp.java
@@ -0,0 +1,12 @@
+package com.connectis.codingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTaskApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTaskApp.class, args);
+    }
+}
